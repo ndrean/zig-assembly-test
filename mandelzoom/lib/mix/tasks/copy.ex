bb63ac64fb1d514605de3e542ff977c9a6c88d8d
Neven DREAN <nevendrean@yahoo.fr>
2024-11-25 10:58:03 +0100

    added Github pages

--- mandelzoom/lib/mix/tasks/copy.ex
+++ mandelzoom/lib/mix/tasks/copy.ex
@@ -6,5 +6,6 @@ defmodule Mix.Tasks.Copy do
     IO.puts("Copying WASM")
     {:ok, ["zoom.wasm"]} = File.ls("../zoomzig/zig-out/bin/")
     :ok = File.cp("../zoomzig/zig-out/bin/zoom.wasm", "./assets/wasm/zoom.wasm")
+    :ok = File.cp("../zoomzig/zig-out/bin/zoom.wasm", "../pages/zoom.wasm")
   end
 end
